eadd6f6609264ccc9e25b6d67f5380f0c1e1b12f
Marlos Pomin <marlospomin@gmail.com>
2019-12-16 23:07:39 -0300

    Update bot

--- index.js
+++ index.js
@@ -1,7 +1,69 @@
+const fs = require('fs')
 const Discord = require('discord.js')
+const { prefix, token } = require('./config.json')
+
 const client = new Discord.Client()
-const config = require('./config.json')
+client.commands = new Discord.Collection()
 
-// Add commands here
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 
-client.login(config.token)
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  client.commands.set(command.name, command)
+}
+
+const cooldowns = new Discord.Collection()
+
+client.once('ready', () => {
+  console.log('Jeeves ready for duty!')
+})
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot || message.channel.type !== 'dm') return
+
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const commandName = args.shift().toLowerCase()
+
+  const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
+
+  if (!command) return
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``
+    }
+
+    return message.channel.send(reply)
+  }
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection())
+  }
+
+  const now = Date.now()
+  const timestamps = cooldowns.get(command.name)
+  const cooldownAmount = (command.cooldown || 3) * 1000
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000
+      return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
+    }
+  }
+
+  timestamps.set(message.author.id, now)
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
+
+  try {
+    command.execute(message, args)
+  } catch (error) {
+    console.error(error)
+    message.reply('There was an error trying to execute that command!')
+  }
+})
+
+client.login(token)
